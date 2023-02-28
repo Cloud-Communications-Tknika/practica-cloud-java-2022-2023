f7e248d5ce244b16296cedff2351b5553f9eb7d7
prubioamzubiri <92910817+prubioamzubiri@users.noreply.github.com>
2023-02-28 10:54:11 +0000

    Añadida documentación a la interfaz de Persistencia.

--- src/main/java/com/cursojava/practica/persistencia/IPersonaGBD.java
+++ src/main/java/com/cursojava/practica/persistencia/IPersonaGBD.java
@@ -11,10 +11,25 @@ public interface IPersonaGBD {
      */
     public Persona getPersona(String id);
 
+    /**
+     * Añade la persona al sistema de persistencia, por ejemplo, una base de datos.
+     * @param persona Objeto de la clase persona que se desea introducir en el sistema de persistencia, debe tener un id distinto a todos los que hay en la persistencia.
+     * @return true si se introducido correctamente, false si existe una Persona con el mismo id en el sistema de presistencia.
+     */
     public bool addPersona(Persona persona);
 
+    /**
+     * Actualiza una Persona del sistema de persistencia.
+     * @param persona Persona que se desea actualizar, debe haber una persona con 
+     * @return devuelve true si la Persona se ha actualizado correctamente, false si no existia ninguna Persona con ese id en el sistema de persistencia.
+     */
     public bool updatePersona(Persona persona);
 
+    /**
+     * Elimina a una Persona del sistema de persistencia.
+     * @param id id de la Persona que se desea eliminar.
+     * @return true si se ha eliminado correctamente, false si no existia ninguna Persona con ese id en el sistema de persistencia.
+     */
     public bool removePersona(String id);
     
 }
